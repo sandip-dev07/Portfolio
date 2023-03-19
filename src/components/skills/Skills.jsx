9aabfc294ecfcb8fe4309c8d1063f45ad0158fc2
SandpiVsCode <108384878+SandipNITA2025@users.noreply.github.com>
2023-03-20 00:55:05 +0530

    Update code

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -89,12 +89,12 @@ const Skills = ({ darkMode }) => {
         </div>
 
         {/* icons */}
-        <div
+        {/* <div
           data-aos="fade-up"
           data-aos-duration="1200"
           className=" container m-auto flex items-center justify-center mt-8 gap-8 md:gap-5"
         >
-          {/* <img
+          <img
             className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
             src="https://img.icons8.com/ios-filled/50/null/c-plus-plus-logo.png"
           />
@@ -115,23 +115,23 @@ const Skills = ({ darkMode }) => {
           <img
             className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
             src="https://img.icons8.com/ios-filled/50/null/git.png"
-            /> */}
-            {/* <img
+            />
+            <img
               className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
               src="https://img.icons8.com/windows/50/null/sass--v1.png"
-            /> */}
-          {/* <img
+            />
+          <img
             className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
             src="https://img.icons8.com/color/48/null/c-plus-plus-logo.png"
-          /> */}
-          {/* <img
+          />
+          <img
             className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]"
             src="https://img.icons8.com/ios-filled/50/null/python.png"
-          /> */}
-          {/* <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/javascript--v1.png"/> */}
-          {/* <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/nodejs.png"/> */}
-          {/* <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png"/> */}
-        </div>
+          />
+          <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/javascript--v1.png"/>
+          <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/color/48/null/nodejs.png"/>
+          <img className="w-[40px] h-[40px] md:w-[35px] md:h-[35px]" src="https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/24/null/external-mongodb-a-cross-platform-document-oriented-database-program-logo-shadow-tal-revivo.png"/>
+        </div> */}
       </div>
     </div>
   );

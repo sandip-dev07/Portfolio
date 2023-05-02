d17ae34496d9580511097bb4461baf3c8db8993f
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-05-02 17:26:14 +0530

    Update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import { Suspense } from "react";
 const Home = React.lazy(() => import("./pages/Home"));
-import UseAnimations from 'react-useanimations';
-import loading from 'react-useanimations/lib/infinity'
+import UseAnimations from "react-useanimations";
+import loading from "react-useanimations/lib/infinity";
+import ClipLoader from "react-spinners/ClipLoader";
+import { BarLoader } from "react-spinners";
 
 const App = () => {
   return (
-    <Suspense fallback={<div className=" h-screen w-full flex items-center justify-center"><UseAnimations animation={loading}
-    size={60} /></div>}>
+    <Suspense
+      fallback={
+        <div className=" h-screen w-full flex items-center justify-center">
+          {/* <UseAnimations animation={loading} size={60} /> */}
+          <BarLoader color="#f6c400" height={4} />
+        </div>
+      }
+    >
       <Home />
     </Suspense>
   );

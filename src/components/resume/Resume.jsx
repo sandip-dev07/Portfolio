159e3ea32e7074cfe06d5aec80d41f9654915a0f
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-05-15 23:23:42 +0530

    Update code

--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -4,17 +4,13 @@ const Resume = () => {
   return (
     <div id="resume" className=" container m-auto   mt-16">
       {/* heading */}
-      <div 
-      data-aos="fade-up"
-       className="relative mb-5">
+      <div data-aos="fade-up" className="relative mb-5">
         <h3 className=" text-3xl font-black text-gray-400 sm:text-2xl">
           Resume
         </h3>
         <span className="h-[1.1px] right-0 absolute w-[92%] bg-gray-300 block"></span>
       </div>
-      <div 
-      data-aos="fade-up"
-       className="left flex-1 w-full">
+      <div data-aos="fade-up" className="left flex-1 w-full">
         <p className=" text-gray-700 font-medium w-[100%]">
           Here are my experiences and qualifications.
         </p>
@@ -23,13 +19,11 @@ const Resume = () => {
       <div className="card-wrapper w-[90%] sm:w-full mt-5 flex md:flex-col sm:gap-5 mx-auto ">
         <div className="left flex-1 flex items-center justify-center">
           <fieldset
-            
             data-aos="zoom-in"
-            
             className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
           >
             <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-yellow-500">
-              Training
+              Internship
             </legend>
             <div className=" relative">
               {/* design */}
@@ -41,16 +35,16 @@ const Resume = () => {
               {/* design */}
               <div className=" flex flex-col gap-1 sm:gap-1 border-2 border-yellow-400 shadow-[0px_0px_16px_1px_rgba(0,0,0,0.1)] p-3 rounded-lg">
                 <h1 className="text-[1.4rem] font-semibold sm:text-xl">
-                  Microsoft Azure AI Fundamentals
+                  Software Developer Intern
                 </h1>
                 <span className=" text-[.9rem] font-semibold text-gray-500 sm:text-base">
-                  Coincent
+                Drafteq Engineering
                 </span>
                 <span className=" text-[.9rem] font-semibold text-yellow-500 sm:text-base">
-                  Sept 2022 - Nov 2022
+                  May 2023 - Present
                 </span>
                 <p className=" text-[.9rem] text-justify break-words text-gray-500">
-                During my Microsoft Azure AI Fundamentals training, I gained a comprehensive understanding of AI concepts, as well as the skills necessary to build and deploy AI solutions on Azure. This included creating machine learning models and chatbots using Azure Machine Learning and Azure Bot Service.
+                I am currently interning at Drafteq Engineering as a Full Stack Developer. My primary focus is on the frontend development using React JS, where I work on creating interactive and user-friendly interfaces. Additionally, I am responsible for building the backend API using Node.js, Express.js, and MongoDB, enabling data storage and retrieval for the application.
                 </p>
               </div>
             </div>
@@ -58,9 +52,7 @@ const Resume = () => {
         </div>
         <div className="right flex-1 flex items-center justify-center">
           <fieldset
-            
             data-aos="zoom-in"
-            
             className=" w-[80%] p-5 py-12 sm:py-8 sm:w-full sm:p-2"
           >
             <legend className=" w-auto ml-[50%] translate-x-[-50%] border-2 border-gray-200 rounded-3xl py-1 px-8 font-semibold text-xl text-yellow-500">
@@ -85,7 +77,12 @@ const Resume = () => {
                   Year 2020 - Year 2024
                 </span>
                 <p className=" text-[.9rem] text-justify text-gray-500">
-                As an undergraduate student, I have a good understanding of web development technologies such as HTML, CSS, Tailwind CSS, JavaScript, and React JS. Additionally, I'm proficient in backend technologies such as Node.js, Express.js, and MongoDB. I also have skills in C++ and problem-solving using Data structures and Algorithms.
+                  As an undergraduate student, I have a good understanding of
+                  web development technologies such as HTML, CSS, Tailwind CSS,
+                  JavaScript, and React JS. Additionally, I'm proficient in
+                  backend technologies such as Node.js, Express.js, and MongoDB.
+                  I also have skills in C++ and problem-solving using Data
+                  structures and Algorithms.
                 </p>
               </div>
             </div>

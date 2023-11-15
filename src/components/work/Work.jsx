d5bea75c6d37ddba881927b63115c954b0b97046
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-11-15 11:09:12 +0530

    loading=lazy

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -20,7 +20,7 @@ const Work = () => {
       </div>
       {/* card */}
       <div className="card-wrapper mx-auto w-[90%] sm:w-fit mt-5">
-        <div className="card-box grid grid-cols-3 space-y-5 space-x-5 w-full md:grid-cols-2 sm:gap-8 sm:grid-cols-1 sm:space-y-0 ">
+        <div className="card-box flex flex-wrap items-center grid-cols-3 gap-5 w-full md:grid-cols-2 sm:gap-8 sm:grid-cols-1 sm:space-y-0 ">
           <WorkCard />
         </div>
       </div>

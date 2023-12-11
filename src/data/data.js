be1e4e0f09acf956b4f621365057b321abf71194
SandipNITA2025 <sarkarsandip7910@gmail.com>
2023-12-11 12:09:52 +0530

    Indohype

--- src/data/data.js
+++ src/data/data.js
@@ -9,8 +9,8 @@ import project8 from "../assets/blog-app.png";
 export const data = [
   {
     id: 1,
-    img: 'https://ik.imagekit.io/imgkitt/tr:w-800/hotel-kusum.jpeg?updatedAt=1683134012764',
-    title: "Hotel Website (Live)",
+    img: "https://ik.imagekit.io/imgkitt/tr:w-800/hotel-kusum.jpeg?updatedAt=1683134012764",
+    title: "Hotel Kusum Palace (Live)",
     desc: "I made this hotel website for a client, using HTML, CSS, JavaScript and it is responsive. It is also live.",
     link: "https://kusumpalace.com/",
     git: "https://github.com/SandipNITA2025/Nepal-hotel-projrct",
@@ -24,7 +24,7 @@ export const data = [
   },
   {
     id: 3,
-    img: 'https://ik.imagekit.io/imgkitt/tr:w-800/weather.jpeg?updatedAt=1683134012969',
+    img: "https://ik.imagekit.io/imgkitt/tr:w-800/weather.jpeg?updatedAt=1683134012969",
     title: "Weather Web",
     desc: "I developed a weather web application using React.js that utilizes the Fetch API and includes PWA (Progressive Web App) functionality.",
     link: "https://weatherwebbappp.netlify.app/",
@@ -33,7 +33,7 @@ export const data = [
   {
     id: 4,
     img: "https://ik.imagekit.io/imgkitt/nike.JPG?updatedAt=1683563311643",
-    title: "Nike Clone (NextJs)",
+    title: "Nike Clone",
     desc: "This Nike clone has been built with the help of Next.js and Tailwind CSS..",
     link: "https://nike-clones.vercel.app/",
     git: "",
@@ -47,7 +47,7 @@ export const data = [
   },
   {
     id: 6,
-    img: 'https://ik.imagekit.io/imgkitt/tr:w-800/ecommerce.jpeg?updatedAt=1683134011136',
+    img: "https://ik.imagekit.io/imgkitt/tr:w-800/ecommerce.jpeg?updatedAt=1683134011136",
     title: "Ecommerce Clone",
     desc: "This is an e-commerce web application built using React JS, SCSS, Redux Toolkit, and a Dummy JSON API. It includes search functionality, as well as the ability to add and remove items from a shopping cart.",
     link: "https://redux-store-app.netlify.app/",
@@ -55,10 +55,18 @@ export const data = [
   },
   {
     id: 7,
-    img: 'https://ik.imagekit.io/imgkitt/mobile.png?updatedAt=1685388082890',
-    title: "Blog Web (MERN Stack)",
+    img: "https://ik.imagekit.io/imgkitt/mobile.png?updatedAt=1685388082890",
+    title: "Blog Website",
     desc: "This is a full-stack web application that uses ReactJS, Tailwind CSS, ExpressJS, and MongoDB. The application features user authentication and enables users to create, edit, and delete their own posts, as well as view posts created by others.",
     link: "https://blog-web-app-delta.vercel.app/",
     git: "https://github.com/SandipNITA2025/MERN-STACK---BLOG-POST-WEB",
+  },
+  {
+    id: 8,
+    img: "https://ik.imagekit.io/imgkitt/indohype.JPG?updatedAt=1702276135123",
+    title: "Indohype (Live)",
+    desc: "Indohype: Your go-to hub for the freshest news and captivating blogs. Stay in the loop with our diverse range of articles covering a wide array of topics. Explore the world of information and entertainment at Indohype today!",
+    link: "https://www.indohype.com/",
+    git: "",
   },
 ];
